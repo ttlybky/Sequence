997a81d20bdd897c2162e96dce4b3fc65b4acdab
huang201530611753 <1256930880@qq.com>
2018-06-23 10:52:13 +0800

    init

---
+++ Sequence.h
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using na,espace std;
+class Sequence
+{public:
+   Sequence(string filename);
+   int length();
+   int numberOf(char base);
+   string longestConsecutive();
+   string longestRepeated();
+
+
+}
