d1295f2ae9cd3263fd5e5fdbd3c479bab3959ad0
huang201530611753 <1256930880@qq.com>
2018-06-23 11:58:16 +0800

    init

--- Sequence.cpp
+++ Sequence.cpp
@@ -1,4 +1,6 @@
 #include<Sequence.h>
+#include<stdafx.h>
+#include<fstream>
 #include<iostream>
 #include<string>
 using namespace std;
@@ -6,12 +8,26 @@ using namespace std;
 Sequence::Sequence(string filename)
 
 {
+char filename1=filename;
 return 0;
 
 }
 Sequence::length()
 {
-cout<<"fuck!! "<<endl;
+  string line;
+  ifstream openfile(filename1);
+  if(!openfile)
+  {cout<<filename1<<"could not be opened.\n";
+  system("pause");
+  return -1;
+  }
+  while(!openfile.eof())
+  {
+   getline(openfile,line)
+   cout<<line<<endl;
+  }
+  openfile.close();
+ 
 return 0;
 }
 Sequence::numberOf(char base)
